6be3e388a141d27c5d0a4758450883ed1a641631
lambangws <macbookairm1@lambangws.local>
2025-08-21 17:40:28 +0800

    esnlint dis

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,5 +12,15 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
+// In a real app, you would also update the mock data "database" here
+const patientInDb = mockScheduledSurgeries.find(p => p.id === id);
+if (patientInDb) {
+  // eslint-disable-next-line no-param-reassign
+  patientInDb.status = newStatus;
+  if (log) {
+    // eslint-disable-next-line no-param-reassign
+    patientInDb.surgeryLog = log;
+  }
+}
 
 export default eslintConfig;
