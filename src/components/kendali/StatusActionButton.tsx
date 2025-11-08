871e52d56f9d1f2103646b646c6ff230e267e29e
lambangws <macbookairm1@lambangws.local>
2025-11-08 21:21:58 +0800

    mengubah path ui kendali

--- src/components/kendali/StatusActionButton.tsx
+++ src/components/kendali/StatusActionButton.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React from 'react';
-import Button from '@/components/ui/button';
+import {Button} from '@/components/ui/ui/button';
 import { Badge } from '@/components/ui/ui/badge';
 import { 
   Hand, PhoneCall, ClipboardCheck, LogIn, 
