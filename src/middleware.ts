88a8662ac3c6e6cde2cda308cd04826814ed9a5a
lambangws <macbookairm1@lambangws.local>
2025-11-09 12:46:42 +0800

    menambahkan kode rare

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,11 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export function middleware(req: NextRequest) {
+export function middleware(_req: NextRequest) {
   return NextResponse.next();
 }
-export const config = { matcher: ["/((?!share/status).*)"] };
+
+export const config = {
+  matcher: ["/((?!share/status).*)"], // semua route kecuali /share/status/*
+};
